45ffa8508be9b6ebcad4ae6991d94c9a3b107457
Cristopher Rosales <crosalesn@anticipa.cl>
2020-03-06 16:28:41 -0300

    Se agregan nuevos servicios

--- src/src/app/services/elecciones.service.ts
+++ src/src/app/services/elecciones.service.ts
@@ -12,9 +12,9 @@ export class EleccionesService {
 
                 var server: string = "DEV";
                 switch(server){
-                  case "DEV"  : this.urlServicios = "http://10.200.7.90/api/usuario/"; break;
-                  case "QA"   : this.urlServicios = "https://eleccioneswsqa.cl/api/usuario/"; break;
-                  case "LOCAL": this.urlServicios = "https://localhost:44349/api/usuario/"; break;
+                  case "DEV"  : this.urlServicios = "http://10.200.7.90/api/"; break;
+                  case "QA"   : this.urlServicios = "https://eleccioneswsqa.cl/api/"; break;
+                  case "LOCAL": this.urlServicios = "https://localhost:44349/api/"; break;
                 }
 
                 
@@ -28,31 +28,111 @@ export class EleccionesService {
    * Metodos de la API
    */
   ObtenerLoginUsuario(parametro): Observable<any>{
-    return this.http.post(this.urlServicios+'ObtenerLoginUsuario',parametro, {headers:this.headers});
+    return this.http.post(this.urlServicios+'usuario/'+'ObtenerLoginUsuario',parametro, {headers:this.headers});
   }
 
   ObtenerParametros(): Observable<any> {
-    return this.http.post(this.urlServicios+'ObtenerParametros','',{headers:this.headers});
+    return this.http.post(this.urlServicios+'parametro/'+'ObtenerParametros','',{headers:this.headers});
   }
 
   ObtenerEstadoInicioFinDia(parametro): Observable<any>{
-    return this.http.post(this.urlServicios+'ObtenerEstadoInicioFinDia',parametro,{headers:this.headers});
+    return this.http.post(this.urlServicios+'registroiniciofindia/'+'ObtenerEstadoInicioFinDia',parametro,{headers:this.headers});
   }
 
   ObtenerPerfiles(): Observable<any> {
-    return this.http.post(this.urlServicios+'ObtenerPerfiles','',{headers:this.headers});
+    return this.http.post(this.urlServicios+'perfil/'+'ObtenerPerfiles'+'ObtenerPerfiles','',{headers:this.headers});
   }
 
   GuardarCoordenadasUsuario(parametro): Observable<any>{
-    return this.http.post(this.urlServicios+'GuardarCoordenadasUsuario',parametro,{headers:this.headers}); 
+    return this.http.post(this.urlServicios+'usuario/'+'GuardarCoordenadasUsuario',parametro,{headers:this.headers}); 
   }
 
   GuardarInicioFinDiaOnline(parametro): Observable<any>{
-    return this.http.post(this.urlServicios+'GuardarInicioFinDiaOnline', parametro,{headers:this.headers});
+    return this.http.post(this.urlServicios+'registroiniciofindia/'+'GuardarInicioFinDiaOnline', parametro,{headers:this.headers});
   }
 
   GuardarInicioFinDiaOffline(parametro): Observable<any>{
-    return this.http.post(this.urlServicios+'GuardarInicioFinDiaOffline', parametro,{headers:this.headers});
+    return this.http.post(this.urlServicios+'registroiniciofindia/'+'GuardarInicioFinDiaOffline', parametro,{headers:this.headers});
   }
+
+  GuardarRegistroSesion(parametro): Observable<any>{
+    return this.http.post(this.urlServicios+'usuario/'+'GuardarRegistroSesion', parametro, {headers: this.headers});
+  }
+
+  ObtenerEntregaFinal(parametro): Observable<any>{
+    return this.http.post(this.urlServicios+'usuario/'+'ObtenerEntregaFinal', parametro, {headers: this.headers});
+  }
+
+  ObtenerTipoLugares(): Observable<any>{
+    return this.http.post(this.urlServicios+'tipolugar/'+'ObtenerTipoLugares', '', {headers: this.headers});
+  }
+
+  ObtenerTipoCarga(): Observable<any>{
+    return this.http.post(this.urlServicios+'tipocarga/'+'ObtenerTipoCarga','', {headers: this.headers});
+  }
+
+  ObtenerRegiones(): Observable<any>{
+    return this.http.post(this.urlServicios+'region/'+'ObtenerRegiones','', {headers: this.headers});
+  }
+
+  ObtenerProvincias(): Observable<any>{
+    return this.http.post(this.urlServicios+'provincia/'+'ObtenerProvincias','', {headers: this.headers});
+  }
+
+  ObtenerMesas(): Observable<any>{
+    return this.http.post(this.urlServicios+'mesa/'+'ObtenerMesas','',{headers: this.headers});
+  }
+
+  ObtenerLugares(): Observable<any>{
+    return this.http.post(this.urlServicios+'lugar/'+'ObtenerLugares','', {headers: this.headers});
+  }
+
+  ObtenerLocalesVotacion(): Observable<any>{
+    return this.http.post(this.urlServicios+'localvotacion/'+'ObtenerLocalesVotacion','', {headers: this.headers});
+  }
+
+  ObtenerEstadoRuta(): Observable<any>{
+    return this.http.post(this.urlServicios+'estadoruta/'+'ObtenerEstadoRuta','',{headers: this.headers});
+  }
+
+  ObtenerEstadoCarga(): Observable<any>{
+    return this.http.post(this.urlServicios+'estadocarga/'+'ObtenerEstadoCarga','',{headers: this.headers});
+  }
+
+  ObtenerComuna(): Observable<any>{
+    return this.http.post(this.urlServicios+'comuna/'+'ObtenerComunas','',{headers: this.headers});
+  }
+
+  ObtenerCircunscripciones(): Observable<any>{
+    return this.http.post(this.urlServicios+'circunscripcion/'+'ObtenerCircunscripciones','',{headers: this.headers});
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
