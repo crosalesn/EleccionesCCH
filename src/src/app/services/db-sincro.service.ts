20ad05155f88654de451d7f00c20d4bbf1764b8e
Cristopher Rosales <crosalesn@anticipa.cl>
2020-03-12 14:14:10 -0300

    Sincronizacion Universo Rutas + Cargas

---
+++ src/src/app/services/db-sincro.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite/ngx';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DbSincroService {
+  private database: SQLiteObject = null;
+  constructor() { }
+
+  setDatabase(database: SQLiteObject){
+    if(this.database === null){
+      this.database = database;
+    }
+  }
+
+  ObtenerRutasNoSincronizadas(): Promise<any> {
+    return this.database.executeSql("SELECT * FROM RUTAS WHERE RTA_SYNC = 0", []);
+  }
+
+  UpdateRutasSincronizadas(): Promise<any> {
+    return this.database.executeSql("UPDATE RUTAS SET RTA_SYNC = 1", []);
+  }
+
+}
